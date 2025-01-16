90527617a7ac244347cc64f88d805b57c1c1a28f
Andreas <andreas.pi.johansson@gmail.com>
2025-01-16 11:30:41 +0100

    stuff and more stuff

--- main.js
+++ main.js
@@ -9,11 +9,21 @@ var newContractor = {
     name: "Andreas",
     department: "HR",
     employeeId: 500,
+    renewEmployment: true,
     employementType: "Contractor",
     salary: { amount: 10000, currency: "SEK" },
     endDate: new Date("2025-01-17"),
 };
-var htmlString = [newIntern, newContractor]
-    .map(function (employee) { return "<tr>\n    <td>".concat(employee.employeeId, "</td>\n    <td>").concat(employee.employementType, "</td>\n    <td>").concat(employee.name, "</td>\n    <td>").concat(employee.department, "</td>\n    <td><input value=\"").concat(employee.salary.amount, "\" type=\"number\"/></td>\n    <td>").concat(employee.endDate ? employee.endDate.toDateString() : "", "</td>\n  </tr>"); })
+var newestContractor = {
+    name: "Elin",
+    department: "HR",
+    employeeId: 501,
+    renewEmployment: false,
+    employementType: "Contractor",
+    salary: { amount: 1000, currency: "SEK" },
+    endDate: new Date("2025-01-22"),
+};
+var htmlString = [newIntern, newContractor, newestContractor]
+    .map(function (employee) { return "<tr>\n    <td>".concat(employee.employeeId, "</td>\n    <td>").concat(employee.employementType, "</td>\n    <td>").concat(employee.name, "</td>\n    <td>").concat(employee.department, "</td>\n    <td><input value=\"").concat(employee.salary.amount, "\" type=\"number\"/></td>\n    <td>").concat(employee.endDate ? employee.endDate.toDateString() : "", "</td>\n    <td><input type=\"checkbox\" ").concat(employee.renewEmployment != null ? employee.renewEmployment ? "checked" : "" : "", "/></td>\n\n  </tr>"); })
     .join("");
-document.querySelector(".employee-table").innerHTML = htmlString;
+document.querySelector(".employee-table").innerHTML = "<thead>\n        <tr>\n          <th>Id</th>\n          <th>Name</th>\n          <th>Age</th>\n          <th>Department</th>\n          <th>Salary</th>\n          <th>End date</th>\n          <th>Renew Status</th>\n\n        </tr>\n      </thead>" + htmlString;
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 interface IEmployee {
   employeeId: number;
   employementType: "Full-time" | "Part-time" | "Intern" | "Contractor";
+  renewEmployment?: boolean;
   endDate?: Date;
   name: string;
   department: "IT" | "HR" | "Support";
@@ -22,12 +23,22 @@ const newContractor: IEmployee = {
   name: "Andreas",
   department: "HR",
   employeeId: 500,
+  renewEmployment: true,
   employementType: "Contractor",
   salary: { amount: 10_000, currency: "SEK" },
   endDate: new Date("2025-01-17"),
 };
+const newestContractor: IEmployee = {
+  name: "Elin",
+  department: "HR",
+  employeeId: 501,
+  renewEmployment: false,
+  employementType: "Contractor",
+  salary: { amount: 1_000, currency: "SEK" },
+  endDate: new Date("2025-01-22"),
+};
 
-const htmlString = [newIntern, newContractor]
+const htmlString:string  = [newIntern, newContractor, newestContractor]
   .map(
     (employee) => `<tr>
     <td>${employee.employeeId}</td>
@@ -36,8 +47,23 @@ const htmlString = [newIntern, newContractor]
     <td>${employee.department}</td>
     <td><input value="${employee.salary.amount}" type="number"/></td>
     <td>${employee.endDate ? employee.endDate.toDateString() : ""}</td>
+    <td><input type="checkbox" ${employee.renewEmployment != null ? employee.renewEmployment ? "checked" : "" : ""}/></td>
+
   </tr>`
   )
   .join("");
 
-document.querySelector(".employee-table")!.innerHTML = htmlString;
+document.querySelector(".employee-table")!.innerHTML = `<thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Department</th>
+          <th>Salary</th>
+          <th>End date</th>
+          <th>Renew Status</th>
+
+        </tr>
+      </thead>` + htmlString;
+
+
